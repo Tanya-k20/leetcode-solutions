8e147be960e1f7784dd4c50d7a6eeea20c44674d
Tanya K <ktanyacontact@gmail.com>
2026-08-28 10:00:14 +0530

    Time: 2 ms (25.94%), Space: 19.4 MB (86.46%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -1,5 +1,21 @@
 class Solution:
     def fizzBuzz(self, n):
+        ans=[]
+        for i in range(1,n+1):
+            if i%3==0 and i%5==0:
+                s="FizzBuzz"
+            elif i%3==0:
+                s="Fizz"
+            elif i%5==0:
+                s="Buzz"
+            else:
+                s=i
+            ans.append(str(s))
+        return ans
         
+                
+
+
+
 
         
