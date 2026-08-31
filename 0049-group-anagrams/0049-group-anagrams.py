c8d0a1b9a7974097e5426206093c00a505bbddea
Tanya K <ktanyacontact@gmail.com>
2026-08-31 10:24:31 +0530

    Time: 11 ms (82.7%), Space: 22.2 MB (53.12%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self,strs):  
+        dic={}
+        for s in strs:
+            key="".join(sorted(s))
+            if key not in dic:
+                dic[key]=[]
+            dic[key].append(s)
+        return list(dic.values())
+
+
+        
