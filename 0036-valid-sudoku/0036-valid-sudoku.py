a1ba9b3d867f9c7d50683db63d18a48d71d29ec4
Tanya K <ktanyacontact@gmail.com>
2026-09-09 20:21:11 +0530

    Time: 4 ms (46.94%), Space: 19.4 MB (35.47%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,15 +1,31 @@
 class Solution:
-    def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows = [[False] * 9 for _ in range(9)]
-        cols = [[False] * 9 for _ in range(9)]
-        boxes = [[False] * 9 for _ in range(9)]
+    def isValidSudoku(self, board):
 
-        for i in range(9):
-            for j in range(9):
-                if board[i][j] != '.':
-                    num = ord(board[i][j]) - ord('1')
-                    boxIndex = (i // 3) * 3 + (j // 3)
-                    if rows[i][num] or cols[j][num] or boxes[boxIndex][num]:
-                        return False
-                    rows[i][num] = cols[j][num] = boxes[boxIndex][num] = True
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        boxes = [set() for _ in range(9)]
+
+        for r in range(9):
+            for c in range(9):
+
+                if board[r][c] == ".":
+                    continue
+
+                num = board[r][c]
+
+                box = (r // 3) * 3 + (c // 3)
+
+                if num in rows[r]:
+                    return False
+
+                if num in cols[c]:
+                    return False
+
+                if num in boxes[box]:
+                    return False
+
+                rows[r].add(num)
+                cols[c].add(num)
+                boxes[box].add(num)
+
         return True
