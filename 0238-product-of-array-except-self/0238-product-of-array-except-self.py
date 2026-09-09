448970ea2a600253bc4c086cd880ccaa98e2a49d
Tanya K <ktanyacontact@gmail.com>
2026-09-09 19:21:13 +0530

    Time: 14 ms (94.29%), Space: 25.4 MB (69.8%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,8 +1,15 @@
 class Solution:
     def productExceptSelf(self, nums):
-        prod, zero_cnt = reduce(lambda a, b: a*(b if b else 1), nums, 1), nums.count(0)
-        if zero_cnt > 1: return [0]*len(nums)
-        for i, c in enumerate(nums):
-            if zero_cnt: nums[i] = 0 if c else prod
-            else: nums[i] = prod // c
-        return nums
+        answer=[1]*len(nums)
+        prefix=1
+        for i in range(len(nums)):
+            answer[i]=prefix
+            prefix*=nums[i]
+        suffix=1
+        for i in range(len(nums)-1,-1,-1):
+            answer[i]*=suffix
+            suffix*=nums[i]
+        return answer
+
+
+            
